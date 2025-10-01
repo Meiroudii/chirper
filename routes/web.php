e0d4701ea2178f3bb263ad113b98352d36999c09
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-10-01 18:48:08 +0900

    feat: add welcome and home blade.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/greet', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/working', function () {
+    return view('gonna_try');
 });
